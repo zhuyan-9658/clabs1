1f7cafbae45f4f5b843a3fdc291238e1d0134115
zhuyan-9658 <1398131736@qq.com>
2019-11-07 06:46:55 -0800

    added a wd.c file

--- lab1/wd.c
+++ lab1/wd.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
 int main()
-{
+{ 
     unsigned   int x = 1;
     int  a =1 ;
     while (x>0){
-    x<<1;++a};
+    x<<1;++a;
+    }
     int n = a-1; 
     int max_int=(1<<n);   
     printf("The max value of int on a machine:%d\n",max_int);
-    return int a ;
+    printf bit [ a]   ;
     
 }
