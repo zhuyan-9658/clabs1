645b7de28d2402437212699698ebc0b8c1eaad51
zhuyan-9658 <1398131736@qq.com>
2019-12-26 07:56:18 -0800

    added a yd.c file

--- lab1/yd.c
+++ lab1/yd.c
@@ -1,16 +1,56 @@
 #include <stdio.h>
+#include <math.h>
+
+int power(int base,int n){
+    int i,p;
+    p=1;
+    for(i=1;i<=n;++i)
+    p=p*base;
+    return p;
+}
 int main()
 {
-    int a =0xfeac;
-    int str[10]={0};
-    int j=0;
-    while(a){
-        str[j]=a%10;
-        a/=10;
-        j++;
+    char m[18]={0};
+    int i=0;
+    char k;
+    int fre = 0;
+    int ghf=-1;
+    unsigned int b=1;
+    printf("please input a positive hexadecimal k(start with 0X/0x)\n");
+    while((k = getchar()) !='\n'){
+        m[i]=k;
+        i++;
+        ghf++;
     }
-    for(int i=j-1;i>=0;i--)
+        if(m[0]!='0'){
+        printf("input error\n");
+        return 0 ;
+        }
+        if(m[1]!='X'&&m[1]!='x'){
+            printf("input error\n");
+            return 0;
+        }
+        i--;
+        while(i>1)
     {
-        printf("%d",str);
+            if(m[i]<='9'&&m[i]>+'0')
+                fre+=(m[i]-'0')*power(16,ghf-i);
+            else if(m[i]>='A'&&m[i]<='F')
+                fre+=(m[i]-'A'+10)*power(16,ghf-i);
+            else if(m[i]>='a'&&m[i]<='f')  
+                fre+=(m[i]+'A'-'a'-'A'+10)*power(16,ghf-i);
+            else{
+                printf("input error\n");
+                return 0;
+                }
+            i--;
     }
+    while(b!=0){
+        b=b<<1;
+    }
+    if(fre>b-1){
+        printf("the max is %u,your input k is too big\n",b-1);
+    }
+    else printf("%d\n",fre);
+    return 0;
 }
